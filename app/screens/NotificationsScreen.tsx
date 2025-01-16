9872c609243282d08fe88521893d1491d7971ae3
TranSon <hoangson145juzk@gmail.com>
2025-01-16 11:15:29 +0700

    merge code

--- app/screens/NotificationsScreen.tsx
+++ app/screens/NotificationsScreen.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity, Alert} from 'react-native';
+import axios from 'axios';
+
 
 export default function NotificationsScreen() {
+
+  
   return (
     <View style={styles.container}>
       <Text style={styles.text}>This is theNotify Screen</Text>
